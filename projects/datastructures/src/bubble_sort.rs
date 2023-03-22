13803cc542021d117fd2d3d68932a2dd1583c0d9
Tim Feid <tim@timfeid.com>
2023-03-22 18:07:13 -0400

    chore: fix

--- projects/datastructures/src/bubble_sort.rs
+++ projects/datastructures/src/bubble_sort.rs
@@ -1,12 +1,10 @@
 fn bubble_sort(arr: &mut [i32]) -> &mut [i32] {
-    let mut remaining = arr.len();
-    while remaining != 0 {
-        for index in 0..remaining - 1 {
-            if arr[index] > arr[index + 1] {
-                arr.swap(index, index + 1);
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
             }
         }
-        remaining -= 1;
     }
 
     arr
