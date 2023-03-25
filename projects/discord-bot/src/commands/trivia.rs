09d46137ef29f21fe39e9ba6df65318cab3a1fd8
Tim Feid <tim@timfeid.com>
2023-03-25 18:34:05 -0400

    feat: actually hook up the test

--- projects/discord-bot/src/commands/trivia.rs
+++ projects/discord-bot/src/commands/trivia.rs
@@ -54,30 +54,43 @@ pub trait TriviaPollHandler {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::{Arc, Mutex};
 
     #[derive(Clone)]
-    struct MockTriviaPollHandler;
+    struct MockTriviaPollHandler {
+        call_count: Arc<Mutex<usize>>,
+    }
+
+    impl MockTriviaPollHandler {
+        fn new() -> Self {
+            MockTriviaPollHandler {
+                call_count: Arc::new(Mutex::new(0)),
+            }
+        }
+    }
 
     impl TriviaPollHandler for MockTriviaPollHandler {
         fn on_poll_updated(&self, poll: &Poll) {
             println!("Mock: Poll updated: {:#?}", poll);
+            *self.call_count.lock().unwrap() += 1;
         }
     }
 
     #[test]
     fn it_does_shit() {
-        // ...
         let mut poll = PollBuilder::new()
             .question("hello world")
             .add_answer("an answer")
             .add_answer("an answer2")
             .make();
 
-        let trivia_poll =
-            TriviaPoll::<MockTriviaPollHandler>::from_poll(poll, MockTriviaPollHandler);
+        let handler = MockTriviaPollHandler::new();
+        let trivia_poll = TriviaPoll::from_poll(poll, handler.clone());
 
         trivia_poll.poll.lock().unwrap().add_answer("tim", '🇦');
         trivia_poll.poll.lock().unwrap().add_answer("bob", '🇦');
         trivia_poll.poll.lock().unwrap().add_answer("joe", '🇦');
+
+        assert_eq!(*handler.call_count.lock().unwrap(), 3);
     }
 }
