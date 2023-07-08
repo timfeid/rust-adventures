d63178afce0dddc29ce7bd33d03ca2d0cbcad840
Tim Feid <tim@timfeid.com>
2023-07-08 09:23:32 -0400

    feat: listen for new articles

--- projects/daily-practice/src/daily20230329.rs
+++ projects/daily-practice/src/daily20230329.rs
@@ -1,5 +1,4 @@
 fn send_message(s: &[char], a: &[i32], mut str: String, k: usize, to: usize) -> String {
-
     let letter = s[k];
     str.push(letter);
 
@@ -7,37 +6,35 @@ fn send_message(s: &[char], a: &[i32], mut str: String, k: usize, to: usize) -> 
         return str;
     }
 
-    send_message(s, a, str, a[k] as usize, to+1)
+    send_message(s, a, str, a[k] as usize, to + 1)
 }
 
 fn decode(s: &[char], a: &[i32]) -> String {
     send_message(s, a, String::from(""), 0, 0)
 }
 
-
 #[test]
 fn code() {
-    let S = ['c', 'd', 'e', 'o'];
-    let A = [3, 2, 0 , 1];
+    let s = ['c', 'd', 'e', 'o'];
+    let a = [3, 2, 0, 1];
 
-    println!("{:#?}", decode(&S, &A));
+    println!("{:#?}", decode(&s, &a));
 
-    assert_eq!(decode(&S, &A), "code");
+    assert_eq!(decode(&s, &a), "code");
 }
-
 
 #[test]
 fn centipede() {
-    let S = ['c', 'd', 'e', 'e', 'n', 'e', 't', 'p', 'i'];
-    let A = [5,2,0,1,6,4,8,3,7];
+    let s = ['c', 'd', 'e', 'e', 'n', 'e', 't', 'p', 'i'];
+    let a = [5, 2, 0, 1, 6, 4, 8, 3, 7];
 
-    assert_eq!(decode(&S, &A), "centipede");
+    assert_eq!(decode(&s, &a), "centipede");
 }
 
 #[test]
 fn bat() {
-    let S = ['b','y','t','d','a','g'];
-    let A = [4,3,0,1,2,5];
+    let s = ['b', 'y', 't', 'd', 'a', 'g'];
+    let a = [4, 3, 0, 1, 2, 5];
 
-    assert_eq!(decode(&S, &A), "bat");
+    assert_eq!(decode(&s, &a), "bat");
 }
--- projects/daily-practice/src/main.rs
+++ projects/daily-practice/src/main.rs
@@ -3,6 +3,7 @@ mod daily20230324;
 mod daily20230325;
 mod daily20230327;
 mod daily20230329;
+mod daily20230708;
 
 fn main() {
     println!("Hello, world!");
